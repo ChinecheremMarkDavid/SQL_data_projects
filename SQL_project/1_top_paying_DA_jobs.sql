c13844b63dad338ab3be8f4055a656e7a6f887fa
Mark-David <marklatitude1@gmail.com>
2024-09-03 09:59:56 +0100

    initial

--- SQL_project/1_top_paying_DA_jobs.sql
+++ SQL_project/1_top_paying_DA_jobs.sql
@@ -1,3 +1,21 @@
+/*
+
+Question: What are the top-paying Data Analyst jobs?
+
+Objective: Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+Approach: Focus on job postings with specified salaries, excluding those with missing data.
+Bonus: Include company names of the top 10 roles to provide a comprehensive view of where the
+best opportunities lie.
+Purpose: Highlight lucrative job options for Data Analysts, showcasing remote work opportunities and potential employers.
+
+*/
+
+
+
+
+
+
+
 SELECT
     job_id,
     job_title,
@@ -22,3 +40,115 @@ ORDER BY
     salary_year_avg DESC
 LIMIT
     10;
+
+/*
+OBSERVATIONS:
+
+Variation by Role: Salaries significantly vary by role, with higher-level positions like
+"Director of Analytics" commanding higher salaries compared to standard data analyst roles.
+
+Variation by Company: Prestigious companies like Meta and AT&T offer competitive salaries,
+though the highest salary is surprisingly offered by Mantys for a "Data Analyst" role.
+
+Salary Spread: There is a wide range in salaries even within similar roles, which may reflect
+different levels of seniority, scope of responsibilities, or company-specific pay scales.
+
+
+
+RESULTS:
+
+[
+  {
+    "job_id": 226942,
+    "job_title": "Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "650000.0",
+    "job_posted_date": "2023-02-20 15:13:33",
+    "company_name": "Mantys"
+  },
+  {
+    "job_id": 547382,
+    "job_title": "Director of Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42",
+    "company_name": "Meta"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 731368,
+    "job_title": "Director, Data Analyst - HYBRID",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189309.0",
+    "job_posted_date": "2023-12-07 15:00:13",
+    "company_name": "Inclusively"
+  },
+  {
+    "job_id": 310660,
+    "job_title": "Principal Data Analyst, AV Performance Analysis",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189000.0",
+    "job_posted_date": "2023-01-05 00:00:25",
+    "company_name": "Motional"
+  },
+  {
+    "job_id": 1749593,
+    "job_title": "Principal Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "186000.0",
+    "job_posted_date": "2023-07-11 16:00:05",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 387860,
+    "job_title": "ERM Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "184000.0",
+    "job_posted_date": "2023-06-09 08:01:04",
+    "company_name": "Get It Recruit - Information Technology"
+  }
+]
+
+
+*/
--- SQL_project/2_top_paying_DS_jobs.sql
+++ SQL_project/2_top_paying_DS_jobs.sql
@@ -1,3 +1,14 @@
+/*
+Question: What are the top-paying Data Scientist jobs?
+
+Objective: Identify the top 10 highest-paying Data Scientist roles that offer remote work options.
+Approach: Focus on job postings with specified salaries, excluding any with missing data.
+Bonus: Include the names of companies offering these top-paying roles for a detailed overview.
+Purpose: Highlight the most lucrative opportunities for Data Scientists, providing insights into flexible work arrangements and potential employers.
+
+*/
+
+
 SELECT
     job_id,
     job_title,
@@ -18,15 +29,106 @@ WHERE
     salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
-
-
-
-
-
-
-
-
-
-
 LIMIT
-    10
+    10;
+
+
+/*
+
+RESULTS:
+[
+  {
+    "job_id": 40145,
+    "job_title": "Staff Data Scientist/Quant Researcher",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "550000.0",
+    "job_posted_date": "2023-08-16 16:05:16",
+    "company_name": "Selby Jennings"
+  },
+  {
+    "job_id": 1714768,
+    "job_title": "Staff Data Scientist - Business Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "525000.0",
+    "job_posted_date": "2023-09-01 19:24:02",
+    "company_name": "Selby Jennings"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "375000.0",
+    "job_posted_date": "2023-07-31 14:05:21",
+    "company_name": "Algo Capital Group"
+  },
+  {
+    "job_id": 1742633,
+    "job_title": "Head of Data Science",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "351500.0",
+    "job_posted_date": "2023-07-12 03:07:31",
+    "company_name": "Demandbase"
+  },
+  {
+    "job_id": 551497,
+    "job_title": "Head of Data Science",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "324000.0",
+    "job_posted_date": "2023-05-26 22:04:44",
+    "company_name": "Demandbase"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "320000.0",
+    "job_posted_date": "2023-03-26 23:46:39",
+    "company_name": "Teramind"
+  },
+  {
+    "job_id": 1161630,
+    "job_title": "Director of Data Science & Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "313000.0",
+    "job_posted_date": "2023-08-23 22:03:48",
+    "company_name": "Reddit"
+  },
+  {
+    "job_id": 129924,
+    "job_title": "Director of Data Science",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "300000.0",
+    "job_posted_date": "2023-01-21 11:09:36",
+    "company_name": "Storm4"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "300000.0",
+    "job_posted_date": "2023-08-06 11:00:43",
+    "company_name": "Walmart"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "300000.0",
+    "job_posted_date": "2023-11-24 14:08:44",
+    "company_name": "Storm5"
+  }
+]
+
+
+
+*/
